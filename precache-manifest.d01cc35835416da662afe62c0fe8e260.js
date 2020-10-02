a1a87913647f274a5a4356bd2fcdfbe35d15e0e4
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-10-02 20:09:58 +0300

    Updates

---
+++ precache-manifest.d01cc35835416da662afe62c0fe8e260.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f482617daa9e508a2649db8f0974da",
+    "url": "/ReacJs-Redux/index.html"
+  },
+  {
+    "revision": "052709c7318f2d2697fd",
+    "url": "/ReacJs-Redux/static/css/main.830af26e.chunk.css"
+  },
+  {
+    "revision": "8eb228d5d699e25d3734",
+    "url": "/ReacJs-Redux/static/js/2.a344bd9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReacJs-Redux/static/js/2.a344bd9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052709c7318f2d2697fd",
+    "url": "/ReacJs-Redux/static/js/main.167aeb5f.chunk.js"
+  },
+  {
+    "revision": "e70548cc09521504da33",
+    "url": "/ReacJs-Redux/static/js/runtime-main.6f64404f.js"
+  },
+  {
+    "revision": "18867fcd477d88985ae98d33173185b6",
+    "url": "/ReacJs-Redux/static/media/anakin_img.18867fcd.jpg"
+  },
+  {
+    "revision": "b189fb6ba69834a5c067896c34f52080",
+    "url": "/ReacJs-Redux/static/media/background-start.b189fb6b.jpg"
+  },
+  {
+    "revision": "3352fdeb8e3413067df241dbfdcd4031",
+    "url": "/ReacJs-Redux/static/media/leia_img.3352fdeb.jpg"
+  },
+  {
+    "revision": "55da1bf0166a5eac98ee67e20a1ac573",
+    "url": "/ReacJs-Redux/static/media/luke_img.55da1bf0.jpg"
+  },
+  {
+    "revision": "40b50c9e452e56b79e8bff1241aa3d54",
+    "url": "/ReacJs-Redux/static/media/palpatine.40b50c9e.jpg"
+  },
+  {
+    "revision": "22de823b055e4f6b460f6a86c66042e8",
+    "url": "/ReacJs-Redux/static/media/rey_avatar.22de823b.jpg"
+  },
+  {
+    "revision": "50847147924e88d4d429dc887b29341a",
+    "url": "/ReacJs-Redux/static/media/rey_img.50847147.jpg"
+  },
+  {
+    "revision": "718524087d6534674e0793834348b97b",
+    "url": "/ReacJs-Redux/static/media/rey_img_2.71852408.jpg"
+  },
+  {
+    "revision": "28584aa096f1429d5c4d19af7957e630",
+    "url": "/ReacJs-Redux/static/media/rey_img_3.28584aa0.jpg"
+  }
+]);
